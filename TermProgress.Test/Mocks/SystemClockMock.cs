e110202bdbf01594eb0fb9bd89e52dc4eed173f1
Gonzalo Ramírez Muñoz de Toro <grrmdt@gmail.com>
2020-06-27 00:51:22 -0300

    Adding tests project. 	- Adding dependencies mocks. 	- Adding tests.

---
+++ TermProgress.Test/Mocks/SystemClockMock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Moq;
+using TermProgress.Library.Helpers;
+
+namespace TermProgress.Tests.Mocks
+{
+    /// <summary>
+    /// Represents a system clock mock.
+    /// </summary>
+    public class SystemClockMock : Mock<ISystemClock>
+    {
+        public SystemClockMock MockNow(DateTime dateTime)
+        {
+            Setup(x => x.Now).Returns(dateTime);
+            return this;
+        }
+    }
+}
