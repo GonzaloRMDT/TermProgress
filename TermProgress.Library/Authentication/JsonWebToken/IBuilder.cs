1896a43dbf53ab840f7f011c13a627dca4fa18e4
Gonzalo Ramírez Muñoz de Toro <grrmdt@gmail.com>
2020-07-05 17:43:19 -0300

    Adding token authentication. 	- Adding JSON Web Token authentication dependencies.

---
+++ TermProgress.Library/Authentication/JsonWebToken/IBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TermProgress.Library.Authentication.JsonWebToken
+{
+    /// <summary>
+    /// <c>IBuilder</c> interface.
+    /// </summary>
+    public interface IBuilder<T>
+    {
+        /// <summary>
+        /// Builds element of type T.
+        /// </summary>
+        /// <returns>Built element of type T.</returns>
+        T Build();
+    }
+}
