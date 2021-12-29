a1e7d719935b0e04c563d2aba7adf688b4e8e3c4
Gonzalo R. Ramírez Muñoz de Toro <grrmdt@gmail.com>
2021-12-29 20:46:38 -0300

    Refactoring authentications. 	- Replacing JWT authentication with ApiKey.

---
+++ TermProgress.Library/Authentications/ApiKey/Attributes/ApiKeyAuthenticationAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace TermProgress.Library.Authentications.ApiKey.Attributes
+{
+    /// <summary>
+    /// Represents an API key authentication attribute.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthenticationAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyHeaderName = "ApiKey";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var apiKeyHeaderValue))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            IConfiguration configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            string apiKey = configuration.GetValue<string>(ApiKeyHeaderName);
+
+            if (!apiKey.Equals(apiKeyHeaderValue))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            await next();
+        }
+    }
+}
