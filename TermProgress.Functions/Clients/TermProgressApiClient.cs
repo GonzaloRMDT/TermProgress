2562dece1607cfb948fc271c34c4f7fd649e71ab
Gonzalo R. Ramírez Muñoz de Toro <grrmdt@gmail.com>
2023-09-07 10:19:44 -0300

    Adding Restsharp as HTTP client.

---
+++ TermProgress.Functions/Clients/TermProgressApiClient.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+using TermProgress.Functions.Options;
+
+namespace TermProgress.Functions.Clients
+{
+    /// <summary>
+    /// Represents a term progress web API client.
+    /// </summary>
+    /// <inheritdoc />
+    public class TermProgressApiClient : ITermProgressApiClient
+    {
+        private readonly RestClient apiClient;
+        private readonly ApplicationOptions applicationOptions;
+
+        /// <summary>
+        /// Class constructor.
+        /// </summary>
+        /// <param name="applicationOptions">A <see cref="IOptions{TOptions}"/> implementation with a generic argument type of <see cref="ApplicationOptions"/>.</param>
+        ///
+        public TermProgressApiClient(IOptions<ApplicationOptions> applicationOptions)
+        {
+            this.applicationOptions = applicationOptions.Value;
+            apiClient = new RestClient(this.applicationOptions.TermProgressApiBaseUrl!);
+        }
+
+        public void Dispose()
+        {
+            apiClient.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task RequestMessageCreationAsync()
+        {
+            RestRequest request = new RestRequest("api/v1/termprogress/twitter")
+                .AddHeader("ApiKey", applicationOptions.ApiKey!);
+            await apiClient.ExecutePostAsync(request);
+        }
+    }
+}
